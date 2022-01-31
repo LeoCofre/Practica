0446ab5ac25772a2da39041ffb274418a5e761c2
LeoCofre <leocofre1981@gmail.com>
2022-01-31 01:36:59 -0300

    Creacion RecyclerView

---
+++ app/src/main/java/com/example/practica/SuperHeroProvider.kt
@@ -0,0 +1,65 @@
+package com.example.practica
+
+class SuperHeroProvider {
+    companion object {
+
+        val superHeroList = listOf<SuperHero>(
+            SuperHero(
+                "KotlinMan",
+                "Jetbrains",
+                "LeoCode",
+                "https://cursokotlin.com/wp-content/ploads/2020/09/Wabp.net-compress-image.jpg"
+            ),
+            SuperHero(
+                "SpiderMan",
+                "Marvel",
+                "Peter Parker",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/spiderman.jpg"
+            ),
+            SuperHero(
+                "DareDevil",
+                "Marvel",
+                "Matthew Michel Murdock",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/daredevil.jpg"
+            ),
+            SuperHero(
+                "Wolverine",
+                "Marvel",
+                "Peter Parker",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/logan.jpg"
+            ),
+            SuperHero(
+                "Batman",
+                "DC",
+                "Bruce Wayne",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/batman.jpg"
+            ),
+            SuperHero(
+                "Thor",
+                "Marvel",
+                "Thor Odison",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/thor.jpg"
+            ),
+            SuperHero(
+                "Flash",
+                "DC",
+                "Jay Garrick",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/flash.jpg"
+            ),
+            SuperHero(
+                "Green Lantern",
+                "DC",
+                "Alan Scott",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/greenlantern.jpg"
+            ),
+            SuperHero(
+                "Wonder Woman",
+                "DC",
+                "Princesa a Caballo",
+                "https://cursokotlin.com/wp-content/ploads/2017/07/spiderman.jpg"
+            )
+
+
+        )
+    }
+}
